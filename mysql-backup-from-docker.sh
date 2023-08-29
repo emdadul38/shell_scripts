90a63a6c7b557e524a51747b68e156670094d62b
Emdadul Huq <emdadul225@gmail.com>
2023-08-29 12:29:40 +0600

    Update mysql-backup-from-docker.sh
    Add commend for create a dumps folder on docker container

--- mysql-backup-from-docker.sh
+++ mysql-backup-from-docker.sh
@@ -3,7 +3,10 @@
 # Please replace ContainerID with docker container id, it will be like f80d57c69b40, 
 # or you can see using the docker ps -a command
 
-# Docker Dumps Command
+# First need to crate a dumps folder on docker container
+docker exec ContainerID mkdir -p dumps
+
+# Docker Dumps Command for backup the database
 docker exec ContainerID sh -c 'mysqldump -uUSER -pPASSWORD db_name > /dumps/db_name.sql'
 
 # Move dump file to the host machine
